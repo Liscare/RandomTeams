96bd5a517c7f4a820aca8a14da8093f2cf3d9797
Liscare <liscare2@protonmail.com>
2020-10-31 18:01:31 +0100

    Save and fetch players list on the device

--- app/src/main/java/com/liscare/android/randomteams/MainActivity.kt
+++ app/src/main/java/com/liscare/android/randomteams/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.liscare.android.randomteams
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.FragmentActivity
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 /**
  * Main activity
- * For now, it is a default activity
+ * Innit the Database
  *
  * @author Lilian Braud
+ * @see DataBase
  */
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (applicationContext.fileList().contains("my_players_group.json")) { // Read saved players
+            DataBase.setContext(applicationContext)
+            applicationContext.openFileInput("my_players_group.json").bufferedReader().useLines {
+                DataBase.setPlayersList(Json.decodeFromString(it.joinToString("")))
+            }
+        } else { // Create an empty file
+            applicationContext.openFileOutput("my_players_group.json", Context.MODE_PRIVATE)
+                .use {
+                    it.write(Json.encodeToString(DataBase.getPlayersList()).toByteArray())
+                }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
